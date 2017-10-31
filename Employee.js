d0a17dfaf8d00b62fe194a383d1a8cec5c8eed5b
Ben Leppard <bleppar@g.clemson.edu>
2017-10-31 11:29:47 -0500

    first commit

--- Employee.js
+++ Employee.js
@@ -3,14 +3,35 @@
 const fs = require('fs')
 
 class Employee {
-  constructor (/* ??? */) {
-    // TODO
+  constructor (name, title, salary) {
+    this.n = name
+    this.t = title
+    this.s = salary
   }
 
-  // TODO ???
-}
+  set title (title) {
+    this.t = title
+  }
 
-// TODO ???
+  set salary (salary) {
+    this.s = salary
+  }
+
+  get salary () {
+    return this.s
+  }
+
+
+
+  static parseFromFilePath (filePath) {
+    let input = JSON.parse(fs.readFileSync(filePath))
+    return new Employee(input.name, input.title, input.salary)
+  }
+
+  promote(title, raise) {
+    this.title = title
+    this.salary = this.salary + raise
+  }}
 
 module.exports = {
   Employee
